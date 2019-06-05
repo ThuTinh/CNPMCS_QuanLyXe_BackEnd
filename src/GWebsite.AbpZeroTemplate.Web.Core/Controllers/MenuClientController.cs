6ce30b21af944893e1c2f468d2c3b6398564d471
ThuTinh <minhtri2041998@gmail.com>
2019-06-06 06:52:36 +0700

    something

--- src/GWebsite.AbpZeroTemplate.Web.Core/Controllers/MenuClientController.cs
+++ src/GWebsite.AbpZeroTemplate.Web.Core/Controllers/MenuClientController.cs
@@ -1,7 +1,12 @@
 ﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using Abp.Notifications;
+using GSoft.AbpZeroTemplate.Authorization.Users;
 using GWebsite.AbpZeroTemplate.Application.Share.MenuClients;
 using GWebsite.AbpZeroTemplate.Application.Share.MenuClients.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace GWebsite.AbpZeroTemplate.Application.Controllers
@@ -9,16 +14,31 @@ namespace GWebsite.AbpZeroTemplate.Application.Controllers
     [Route("api/[controller]/[action]")]
     public class MenuClientController : GWebsiteControllerBase
     {
+        private readonly INotificationPublisher _notificationPublisher;
+        private readonly IRepository<User, long> userRepository;
+        
         private readonly IMenuClientAppService _menuClientAppService;
 
-        public MenuClientController(IMenuClientAppService menuClientAppService)
+        public MenuClientController(IMenuClientAppService menuClientAppService,
+            IRepository<User, long> userRepository,
+            INotificationPublisher notificationPublisher)
         {
             _menuClientAppService = menuClientAppService;
+            _notificationPublisher = notificationPublisher;
+            this.userRepository = userRepository;
         }
 
         [HttpGet]
-        public string GetTest()
+        public async Task<string> GetTest()
         {
+            var users = await userRepository.GetAll().ToListAsync();
+            await _notificationPublisher.PublishAsync(
+                "Chào thu tình",
+                new MessageNotificationData("Đây là nội dung của thông báo"),
+                severity: NotificationSeverity.Success,
+                userIds: users.Select(x => x.ToUserIdentifier()).ToArray()
+                );
+
             return "Test";
         }
 
